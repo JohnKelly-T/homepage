99ad6ea6d12207e03d62e72800f3610df2378c5a
john-kelly <johnkellytemena@gmail.com>
2025-07-28 16:37:34 +0800

    Add functionality to hide nav on scrolldown

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import './styles/reset.css';
 import './styles/global.css';
+
+// code for hiding and showing nav based on scroll
+let lastScrollPos = 0;
+let nav = document.querySelector("nav");
+
+document.addEventListener('scroll', () => {
+  // if scrolling down hide nav else show nav
+  if (window.scrollY > lastScrollPos) {
+    nav.classList.add("hide");
+  } else {
+    nav.classList.remove("hide");
+  }
+
+  // update last scroll pos
+  lastScrollPos = window.scrollY;
+});
